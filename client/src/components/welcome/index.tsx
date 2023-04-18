7b7b7654a3ffcc072dff984dc66b12c97816ef98
Garrison <garrison@pc103895.PKRVMN.local>
2023-04-18 11:21:10 +0300

    feat: сделана логика enebled кнопки подключения к чату

--- client/src/components/welcome/index.tsx
+++ client/src/components/welcome/index.tsx
@@ -1,13 +1,26 @@
-import { FC } from 'react'
+import { ChangeEvent, FC, useState } from 'react'
 import welcome from '@/assets/img/welcome.jpg'
 import { IProps } from './props'
 
 
-export const Welcome: FC<IProps> = ({ onAbort }) => (
-  <div>
-    <img src={welcome} alt="welcome" width="728px" height="409px" />
-    <input />
-    <button>Yes</button>
-    <button onClick={onAbort}>No</button>
-  </div>
-)
+export const Welcome: FC<IProps> = ({ onAbort }) => {
+
+  const [name, setName] = useState('')
+  const [isDisabled, setIsDisabled] = useState(true)
+
+
+  const handleInput = (event: ChangeEvent<HTMLInputElement>) => {
+    setName(event.target.value)
+    setIsDisabled(!event.target.value)
+  }
+
+
+  return (
+    <div>
+      <img src={welcome} alt="welcome" width="728px" height="409px" />
+      <input value={name} onChange={handleInput} />
+      <button disabled={isDisabled}>Yes</button>
+      <button onClick={onAbort}>No</button>
+    </div>
+  )
+}
