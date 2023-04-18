a8b89ffcba104c1e9f76e25ac732054debaa9452
Garrison <garrison@pc103895.PKRVMN.local>
2023-04-18 14:31:12 +0300

    feat: стилизован компонент Welcome

--- client/src/components/welcome/index.tsx
+++ client/src/components/welcome/index.tsx
@@ -1,13 +1,11 @@
 import { ChangeEvent, FC, useState } from 'react'
 import welcome from '@/assets/img/welcome.jpg'
 import { IProps } from './props'
-
+import styles from './index.module.css'
 
 export const Welcome: FC<IProps> = ({ onAbort, onConnect }) => {
-
   const [name, setName] = useState('')
   const [isDisabled, setIsDisabled] = useState(true)
-
 
   const handleInput = (event: ChangeEvent<HTMLInputElement>) => {
     setName(event.target.value)
@@ -18,13 +16,17 @@ export const Welcome: FC<IProps> = ({ onAbort, onConnect }) => {
     onConnect(name)
   }
 
-
   return (
-    <div>
-      <img src={welcome} alt="welcome" width="728px" height="409px" />
-      <input value={name} onChange={handleInput} />
-      <button disabled={isDisabled} onClick={handleConnect}>Yes</button>
-      <button onClick={onAbort}>No</button>
+    <div className={styles.container}>
+      <h1 className={styles.title}>Кто Вы, мистер Андерсон?</h1>
+      {name.length > 2 && <img src={welcome} alt="welcome" width="728px" height="409px" />}
+      <input value={name} onChange={handleInput} className={styles.input_name} />
+      <button disabled={isDisabled} onClick={handleConnect} className={styles.btn_connect}>
+        Yes
+      </button>
+      <button onClick={onAbort} className={styles.btn_abort}>
+        No
+      </button>
     </div>
   )
 }
